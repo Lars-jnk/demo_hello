3deb52188218f15e311f46b1739d479a5879a3a2
Lars-jnk <39338473+Lars-jnk@users.noreply.github.com>
2018-09-10 11:31:53 +0200

    hello lbl

--- src/main/java/de/freizeitbaltrum/cdi/MainView.java
+++ src/main/java/de/freizeitbaltrum/cdi/MainView.java
@@ -1,6 +1,7 @@
 package de.freizeitbaltrum.cdi;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -18,7 +19,7 @@ public class MainView extends VerticalLayout {
     public MainView() {
         Button button = new Button("Click me",
                 event -> Notification.show(messageBean.getMessage()));
-        add(button);
+        add(new Label("Hallo Welt"), button);
     }
 
 }
